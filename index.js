5dc9f5f8669513fdee5d7afb09ba8d1dea84e30c
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-09-12 08:47:31 +0600

    Added Get Data & Post Data

--- index.js
+++ index.js
@@ -2,14 +2,48 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 var cors = require('cors')
- 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 app.use(cors())
 app.use(express.json())
 require('dotenv').config()
 
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ghravcb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+  try {
+    await client.connect();
+    const feedbackCollection = client.db('Feedback-form').collection('form-data');
+
+    app.get('/form-data', async (req,res) =>{
+      const query = {};
+      const cursor = feedbackCollection.find(query);
+      const data = await cursor.toArray();
+      res.send(data);
+    });
+
+    app.post('/feedback-form', (req, res) =>{
+      const addData = req.body;
+      // const result = feedbackCollection.insertOne(addData)
+      console.log(addData)
+
+    })
+    
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Welcome to feedback form')
 })
 
 app.listen(port, () => {
